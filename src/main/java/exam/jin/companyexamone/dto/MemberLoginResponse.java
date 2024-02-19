899c5c12266965de460fceb53a23f4e4e8ddb3fd
jinsejin <jintyjin15@gmail.com>
2024-02-19 16:54:49 +0900

    feat: 로그인 로직 추가

---
+++ src/main/java/exam/jin/companyexamone/dto/MemberLoginResponse.java
@@ -0,0 +1,16 @@
+package exam.jin.companyexamone.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberLoginResponse {
+
+    private String loginId;
+
+    private String accessToken;
+
+    public MemberLoginResponse(MemberLoginResult result) {
+        this.loginId = result.getLoginId();
+        this.accessToken = result.getAccessToken();
+    }
+}
