d43db596129165dbcc0ca8a4e805808008747766
jinsejin <jintyjin15@gmail.com>
2024-02-19 11:08:54 +0900

    feat: Member entity

---
+++ src/main/java/exam/jin/companyexamone/entity/Member.java
@@ -0,0 +1,46 @@
+package exam.jin.companyexamone.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "member_loginId")
+    private String loginId;
+
+    @Column(name = "member_password")
+    private String password;
+
+    @Column(name = "member_username")
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String loginId, String password, String username) {
+        this.loginId = loginId;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
