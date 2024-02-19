4f0ef0b5520c263c91e856faeef86f83fdcec108
jinsejin <jintyjin15@gmail.com>
2024-02-19 10:59:11 +0900

    first commit

---
+++ src/test/java/exam/jin/companyexamone/CompanyExamOneApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.jin.companyexamone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyExamOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
